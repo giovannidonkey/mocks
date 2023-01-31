9c86439c31f2c2e71953467a4c70f527839f2fd0
Ryan Culp <105468921+giovannidonkey@users.noreply.github.com>
2023-01-30 22:39:01 -0500

    Add files via upload

---
+++ vehicles.js
@@ -0,0 +1,46 @@
+class Vehicle {
+    constructor(make, model, year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    honk(){
+        return "BEEP!"
+    }
+
+    toString(){
+        return `The vehicle is a ${make} ${model} made in ${year}.`;
+    }
+
+}
+
+class Car extends Vehicle{
+    constructor(make, model, year){
+        super(make,model,year)
+        this.numTires = 4;
+    }
+}
+
+class Motorcycle extends Vehicle{
+    constructor(make, model, year){
+        super(make,model,year)
+        this.numTires = 2;
+    }
+    revEngine(){
+        return 'VROOMMM VROOM';
+    }
+}
+
+class Garage{
+    constructor(cap){
+        this.spots = [];
+        this.cap = cap;
+    }
+
+    add(vehicle){
+        if(vehicle instanceof Vehicle) return 'Only Vehicles, fool!';
+        if(this.spots.length >= this.cap) return 'Sorry we are at capacity.';
+        this.spots.push(vehicle);
+    }
+}
